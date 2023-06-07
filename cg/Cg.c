91553d8f6079e4bac4da1f36552a602b738b4258
Tabuchi@nada <tabuchi@nada.co.jp>
2023-06-08 08:40:25 +0900

    first

---
+++ cg/Cg.c
@@ -0,0 +1,17 @@
+/************************************************
+*												*
+*		TP-1132NB CHRACTOR GENERATER			*
+*												*
+************************************************/
+
+#pragma section CG
+
+#include "Cg8x16d.c"		/*  5120byte	*/
+#include "Cg12x24d.c"		/*  7680byte	*/
+#include "Cg16x16c.c"		/*  5120byte	*/
+#include "Cg24x24a.c"		/* 15360byte	*/
+#include "Script.c"			/*   924byte	*/
+#include "Cg48x48i.c"		/* 10386byte	*/
+
+#pragma section				/* change normal section	*/
+/*	end of file		*/
